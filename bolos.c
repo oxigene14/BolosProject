7b7369112048ea97ece031385af2425f4f81482d
JC <jean.camille.dordain@gmail.com>
2022-11-09 16:19:09 +0100

    commit test

--- bolos.c
+++ bolos.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World! \n");
-   return 0;
+#include <sys/types.h>
+#include <unistd.h>
+void forkexample()
+{
+int processID = fork();
+
+    if(processID>0)
+    {
+        printf("fork() returned a +ve value. This is the parent process, with ID: %d \n",getpid());
+    }
+    else if(processID==0)
+    {
+        printf("fork() returned a 0 value. This is a newly created child process with ID: %d \n",getpid());
+        printf("The parent process of this child process has the ID: %d\n",getppid());
+    }
+    else
+    {
+        printf("fork() returned a -ve value, so the fork system called failed and the child process could not be created\n");
+    }
+    printf("This is a single print statement. If the fork() system call was successful, both the parent and child process will run concurrently, and this statement will print twice.\n");
+}
+
+
+int main()
+{
+    forkexample();
+    return 0;
 }
