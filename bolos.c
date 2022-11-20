0ba757ad8a1bb15932c6599a11b288ebe065f749
oxigene14 <jean-camille@dordain.org>
2022-11-20 20:01:20 +0100

    commit

--- bolos.c
+++ bolos.c
@@ -8,9 +8,10 @@
 
 void forkexample()
 {
-    // Declaration of the two process p and a
-    pid_t process_p, process_a;
+    // Declaration of all the processes in the program
+    pid_t process_p, process_a, process_b, process_h, process_e, process_i, process_c;
 
+    char word[256];
     // Fork from the program and creation of the process_p
     process_p = fork();
 
@@ -18,12 +19,6 @@ void forkexample()
     // PID of the child is 0
     switch(process_p)
     {
-        // If an error ocurred while forking then print an error message to stderr
-        case -1:
-            perror("fork failed for the process p");  
-            break;
-        // If this is the process_p code
-        // PID of the child is 0
         case 0:
             // Fork of the process_a based on the process_p
             process_a = fork();
@@ -39,10 +34,29 @@ void forkexample()
                 // PID of the child is 0
                 case 0:
                     printf("I am the child a from the parent p with pid %d\n", (int)getpid());
-                    
-                    sleep(10);
+                    // Fork of the process_b based on the process_a
+                    process_b = fork();
+                    // Switch case for the process_b
+                    // PID of the child is 0
+                    switch(process_b)
+                    {
+                        // If an error ocurred while forking then print an error message to stderr
+                        case -1:
+                            perror("fork failed for the process b");  
+                            break;
+                        // If this is the process_b code
+                        // PID of the child is 0
+                        case 0:
+                            printf("I am the child b from the parent a with pid %d\n", (int)getpid());
+                            pause();
+                            printf("The pid of the parent a is %d\n", (int)getppid());
+                            break;
+                        // If this is not the process_b code then it is the process_a code
+                        default:
+                            wait(NULL);
+                    }
+                    pause();
                     printf("The pid of the parent p is %d\n", (int)getppid());
-                    printf("Child exiting...\n");
                     break;
                 // If this is not the process_a code then it is the process_p code
                 default:
@@ -53,6 +67,7 @@ void forkexample()
             }
         // if it is the main programm, then it will wait for the child process to finish
         default:
+            scanf("%s" , word) ;
             wait(NULL);
     }
 }
